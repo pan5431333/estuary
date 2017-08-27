a968ad8ffde9303d251f0ab6e374170e9529a05e
mengpan <pan5431333@hotmail.com>
2017-08-27 11:23:12 +0800

    config_project

---
+++ src/test/scala/MainTest.scala
@@ -0,0 +1,30 @@
+
+import breeze.linalg.{DenseMatrix, DenseVector}
+import breeze.numerics.pow
+import org.mengpan.deeplearning.helper.GasCensorDataHelper
+import org.mengpan.deeplearning.model.LogisticRegressionModel
+
+object MainTest extends App {
+//  val lrModel: LogisticRegressionModel = new LogisticRegressionModel()
+//    .setIterationTime(3000)
+//    .setLearningRate(0.0001)
+//
+//  println("Learning rate is: " + lrModel.getLearningRate)
+//  println("Iteration time is: " + lrModel.getIterationTime)
+//
+//
+//  val w = DenseVector.ones[Double](10)
+//  val X = DenseMatrix.zeros[Double](10, 10000)
+//  val b = 0.0
+//  println(w /:/ 2.0)
+
+//  val data = GasCensorDataHelper.getAllData
+//  println(data)
+
+//  val testA = DenseMatrix.ones[Double](2, 3) * 2.0
+//  println(testA)
+//  println(pow(testA, 2))
+
+  println(pow(10.0, -10).toDouble)
+
+}
