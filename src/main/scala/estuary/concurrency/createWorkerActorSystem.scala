7f15a7b45a56a8a3e6e7e312c3a46571ea9f0324
han_chen <han_chen@ctrip.com>
2017-10-24 11:59:43 +0800

    cross machines distributed optimizer

--- src/main/scala/estuary/concurrency/createWorkerActorSystem.scala
+++ src/main/scala/estuary/concurrency/createWorkerActorSystem.scala
@@ -6,7 +6,8 @@ import com.typesafe.config.ConfigFactory
 object createWorkerActorSystem {
   def main(args: Array[String]): Unit = {
     val systemName = if (args == null) "Worker" else args(0)
+    val configPath = if (args.length > 1) args(1) else "WorkerActorSystem"
 
-    ActorSystem(systemName, ConfigFactory.load("WorkerActorSystem"))
+    ActorSystem(systemName, ConfigFactory.load(configPath))
   }
 }
